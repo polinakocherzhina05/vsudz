150351c660fed23ee5b86b01bb0a77fb1d196b07
polinakocherzhina05 <143869729+polinakocherzhina05@users.noreply.github.com>
2023-09-30 12:41:10 +0300

    Update дз 2.5.py

--- 2/дз 2.5.py
+++ 2/дз 2.5.py
@@ -1,7 +1,7 @@
 x = int(input('Введите пятизначное число'))
-while x > 99999 or x <= 9999:
+while x >= 10000 and x <= 99999:
     print('Число не коректно для данной задачи')
     x = int(input('Введите пятизначное число:'))
 x = str(x)
-for a in(x):
+for a in x:
     print(a)

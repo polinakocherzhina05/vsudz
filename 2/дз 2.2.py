637e34568773ca04c80d3936fdb492bcd98933bb
polinakocherzhina05 <143869729+polinakocherzhina05@users.noreply.github.com>
2023-10-12 21:34:01 +0300

    Update дз 2.2.py

--- 2/дз 2.2.py
+++ 2/дз 2.2.py
@@ -1,6 +1,5 @@
-a, b = input(), []
-while a:
-    b.append(a)
-    a = input()
-b.sort(reverse=True)
-print(''.join(b))
+numbers = input("Введите список чисел через пробел: ").split()
+numbers.sort(reverse=True)
+numbers_as_strings = [str(num) for num in numbers]
+result = "".join(numbers_as_strings)
+print("Максимально возможное число:", result)

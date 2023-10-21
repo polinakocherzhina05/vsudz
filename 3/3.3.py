d0aaada8187d70c1ec6738a25dadd30c204f3443
polinakocherzhina05 <143869729+polinakocherzhina05@users.noreply.github.com>
2023-10-21 13:08:53 +0300

    Update 3.3.py

--- 3/3.3.py
+++ 3/3.3.py
@@ -1,7 +1,8 @@
-def f(n):
-    fib = [0, 1]
-    for i in range(2, n):
-        fib.append(fib[i-1] + fib[i-2])
-    print(' '.join(str(x) for x in fib[:n]))
-n = int(input("Введите количество чисел Фибоначчи: "))
-f(n)
+def fib(n):
+    f = [0, 1, 1]
+    for _ in range(n - 2):
+        f.append(f[-2] + f[-1])
+    return f[:n]
+
+n = int(input())
+print(fib(n))

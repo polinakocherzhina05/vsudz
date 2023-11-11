7e275614c12d7269a55350a810e1ae69ca1b5d64
polinakocherzhina05 <143869729+polinakocherzhina05@users.noreply.github.com>
2023-11-11 12:40:38 +0300

    Update i.py

--- tests/five/i.py
+++ tests/five/i.py
@@ -1,21 +1,16 @@
-from gg.one import ff
-
-
-def binp(place :list, num):
-    if place == []:
-        return
-    mid = len(place) // 2
-    small = 0
-    big = len(place) -1
-    while place[mid] != num and small <= big:
-        if num > place[mid]:
-            small = mid + 1
+def binary_search(sequence, target):
+    low, high = 0, len(sequence) - 1
+    result = None
+    while low <= high:
+        mid = (low + high) // 2
+        if sequence[mid] >= target:
+            result = mid
+            high = mid - 1
         else:
-            big = mid - 1
-        mid = (small + big) // 2
-    if small <= big:
-        return mid
+            low = mid + 1
+    return result
 
 
-if __name__ == "__main__":
-    print(binp(list(map(float, ff(input()))), int(input())))
+if __name__ == '__main__':
+
+    print(binary_search([-1, 1, 1, 1, 1, 1, 9], 9))

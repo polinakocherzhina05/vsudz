a6e60d575c0e22275904fbcef31885fe4d1360ed
polinakocherzhina05 <143869729+polinakocherzhina05@users.noreply.github.com>
2023-11-11 13:51:46 +0300

    Update i.py

--- tests/five/i.py
+++ tests/five/i.py
@@ -1,16 +1,13 @@
-def binary_search(sequence, target):
-    low, high = 0, len(sequence) - 1
+def binnary_search(num, lst):
     result = None
+    low, high = 0, len(lst) - 1
     while low <= high:
         mid = (low + high) // 2
-        if sequence[mid] >= target:
+        if lst[mid] == num:
             result = mid
+            high = mid - 1
+        elif lst[mid] > num:
             high = mid - 1
         else:
             low = mid + 1
     return result
-
-
-if __name__ == '__main__':
-
-    print(binary_search([-1, 1, 1, 1, 1, 1, 9], 9))

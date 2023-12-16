73af10cd0e9c48d70fbe28647895c64d0b2158f8
polinakocherzhina05 <143869729+polinakocherzhina05@users.noreply.github.com>
2023-12-16 12:21:12 +0300

    Update test_l.py

--- tests/test_l.py
+++ tests/test_l.py
@@ -1,52 +1,25 @@
 import pytest
-from seven.h import bfs
+from dz7.hw7 import search
 
 
-@pytest.mark.parametrize("graph, start, target, expected, f", [
-    (
-            {'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F'], 'D': ['B'], 'E': ['B', 'F'], 'F': ['C', 'E']}, 'A',
-            'A', 0,
-            lambda n, t: n == t),
-    (
-            {'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F'], 'D': ['B'], 'E': ['B', 'F'], 'F': ['C', 'E']}, 'A',
-            'B', 1,
-            lambda n, t: n == t),
-    (
-            {'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F'], 'D': ['B'], 'E': ['B', 'F'], 'F': ['C', 'E']}, 'A',
-            'C', 1,
-            lambda n, t: n == t),
-    (
-            {'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F'], 'D': ['B'], 'E': ['B', 'F'], 'F': ['C', 'E']}, 'A',
-            'D', 2,
-            lambda n, t: n == t),
-    (
-            {'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F'], 'D': ['B'], 'E': ['B', 'F'], 'F': ['C', 'E']}, 'A',
-            'E', 2,
-            lambda n, t: n == t),
-    (
-            {'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F'], 'D': ['B'], 'E': ['B', 'F'], 'F': ['C', 'E']}, 'A',
-            'F', 2,
-            lambda n, t: n == t),
-    ({'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F'], 'D': ['B'], 'E': ['B', 'F'], 'F': ['C', 'E']}, 'A', 'G',
-     None, lambda n, t: n == t),
-    ({'A': ['B', 'E'], 'B': ['A', 'C'], 'C': ['B', 'D'], 'D': ['C', 'E'], 'E': ['A', 'D']}, 'A', 'D', 2,
-     lambda n, t: n == t),
-    ({'A': ['B', 'E'], 'B': ['A', 'C'], 'C': ['B', 'D'], 'D': ['C', 'E'], 'E': ['A', 'D']}, 'B', 'E', 2,
-     lambda n, t: n == t),
-    ({'A': ['B', 'E'], 'B': ['A', 'C'], 'C': ['B', 'D'], 'D': ['C', 'E'], 'E': ['A', 'D']}, 'C', 'D', 1,
-     lambda n, t: n == t),
-    ({'A': ['B', 'E'], 'B': ['A', 'C'], 'C': ['B', 'D'], 'D': ['C', 'E'], 'E': ['A', 'D']}, 'D', 'E', 1,
-     lambda n, t: n == t),
-    ({'A': ['B', 'E'], 'B': ['A', 'C'], 'C': ['B', 'D'], 'D': ['C', 'E'], 'E': ['A', 'D']}, 'E', 'A', 1,
-     lambda n, t: n == t),
-    ({'A': ['B', 'E'], 'B': ['A', 'C'], 'C': ['B', 'D'], 'D': ['C', 'E'], 'E': ['A', 'D']}, 'A', 'F', None,
-     lambda n, t: n == t),
-    ({}, "A", "F", None, lambda n, t: n == t),
-    ({0: [1, 2], 1: [0, 3, 4], 2: [0, 5, 6], 3: [1], 4: [1, 6], 5: [2], 6: [2, 4]}, 0, 0, 0,
-     lambda n, t: n % 2 == t % 2),
-    ({}, 0, 1, None, lambda n, t: n % 2 == t % 2),
-    ({0: [1, 2], 1: [0, 3, 4], 2: [0, 5, 6], 3: [1], 4: [1, 6], 5: [2], 6: [2, 4]}, 0, 2, 1,
-     lambda n, t: n % 3 == t % 3),
-])
-def test_bfs(graph, start, target, expected, f):
-    assert bfs(graph, start, target, f) == expected
+def condition(this, cur):
+    return this == cur
+
+
+def cc(gr, cur):
+    return bool(gr)
+        
+
+
+@pytest.mark.parametrize(
+    ('gr', 'st', 'cur', 'forward', 'f'),
+    [
+        ({'A': [], 'B': [], 'C': []}, 'A', 'C', None, condition),
+        ({'A': ['B', 'C'], 'B': ['D'], 'C': ['D'], 'D': ['E'], 'E': []}, 'A', 'E', 3, condition),
+        ({'A': ['B', 'C'], 'B': ['D', 'A'], 'C': ['D', 'E'], 'D': ['E']}, 'A', 'A', 0, condition),
+        ({'' : ['A'], 'A' : ['']}, '', 'A', 1, condition),
+        ({}, 'A', 'A', None, cc)
+    ]
+)
+def test(gr, st, cur, forward, f):
+    assert search(gr, st, cur, f) == forward

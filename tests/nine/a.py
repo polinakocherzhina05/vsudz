29baf36b21c58530adc588b68eba67850eddf1eb
polinakocherzhina05 <143869729+polinakocherzhina05@users.noreply.github.com>
2024-02-24 16:23:53 +0300

    Update a.py

--- tests/nine/a.py
+++ tests/nine/a.py
@@ -1,25 +1,23 @@
 class Fraction:
     def __init__(self, numerator=0, denominator=1):
-        self.numerator = numerator
-        self.denominator = denominator
-        self.validate()
-        if self.numerator == self.denominator:
-            print("Число целое")
-        else:
-            print("Число не целое")
+        self.numerator, self.denominator = self.validate(numerator, denominator)
 
     def input_fraction(self):
-        self.numerator = int(input("Введите числитель: "))
-        self.denominator = int(input("Введите знаменатель: "))
-        self.validate()
+        self.numerator, self.denominator = self.validate(*input("Введите числитель и знаменатель через пробел: ").split(' '))
 
     def __str__(self):
         return f"{self.numerator}/{self.denominator}"
 
-    def validate(self):
-        if self.denominator == 0:
-            raise ValueError("Знаменатель не может быть равен нулю")
+    @staticmethod
+    def validate(num, den):
+        if den == 0:
+            raise ZeroDivisionError("Знаменатель не может быть равен нулю")
+        if str(num).isdigit() and str(den).isdigit():
+            return int(num), int(den)
+        raise ValueError('Неверный формат данных')
 
-# t = Fraction
-# t.input_fraction
-# print(t)
+
+f = Fraction(12, 13)
+f.validate(12, 1)
+f.input_fraction()
+print(type(f.numerator))
